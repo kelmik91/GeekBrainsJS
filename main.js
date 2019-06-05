4fc2452a9cbc6da239f89cd777aa2feb0c7fa0c2
kelmik91 <kelmik91@me.com>
2019-06-05 20:03:22 +0300

    Домашка по первому уроку JS

---
+++ main.js
@@ -0,0 +1,22 @@
+// Задание 1
+var temperature_celsius = +prompt("Введите температуру в цельсиях");
+var temperature_fahrenheit = (9/5)*temperature_celsius+32;
+alert(temperature_celsius + " градуса Цельсия - это " + temperature_fahrenheit + " градусов по Фаренгейту");
+
+// Задание 3
+var admin;
+var name;
+name = "Василий";
+admin = name;
+alert(admin);
+
+/* Задание 4
+
+Тексту - "1000108"
+
+*/
+
+var one = 1000;
+var two = "108";
+var thee = one + two;
+alert(thee);
